6a973943cd354406b1730ea8a42587e5fb2a9781
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-12-21 19:23:36 +0100

    Create hello.s

---
+++ code/helloworld/hello.s
@@ -0,0 +1,23 @@
+// Hudson Schumaker
+// Assembler program to print "Hello World!"
+// to stdout.
+//
+// X0-X2 - parameters to linux function services
+// X8 - linux function number, for iOS/macOS X16
+//
+
+.data
+helloworld: .ascii  "Hello World!\n"
+
+.text
+.global main	           
+
+_main: 
+  mov	X0, #1	          // 1 = StdOut
+	ldr	X1, =helloworld   // string to print
+	mov	X2, #13	          // length of our string
+	mov	X8, #64	          // linux write system call
+	svc	0 	              // Call linux to output the string
+	mov X0, #0            // Use 0 return code
+  mov X8, #93           // Service command code 93 terminates this program
+  svc 0                 // Call linux to terminate the program
