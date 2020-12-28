06f20a02872b5a942889636cc7640a1dea3a1755
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-12-28 19:39:10 +0000

    Update hello.s

--- code/helloworld/hello.s
+++ code/helloworld/hello.s
@@ -13,11 +13,11 @@ helloworld: .ascii  "Hello World!\n"
 .global main	           
 
 _main: 
-  mov	X0, #1	          // 1 = StdOut
-	ldr	X1, =helloworld   // string to print
-	mov	X2, #13	          // length of our string
-	mov	X8, #64	          // linux write system call
-	svc	0 	              // Call linux to output the string
-	mov X0, #0            // Use 0 return code
-  mov X8, #93           // Service command code 93 terminates this program
-  svc 0                 // Call linux to terminate the program
+    mov	X0, #1	          // 1 = StdOut
+    ldr	X1, =helloworld   // string to print
+    mov	X2, #13	          // length of our string
+    mov	X8, #64	          // linux write system call
+    svc	0 	          // Call linux to output the string
+    mov X0, #0            // Use 0 return code
+    mov X8, #93           // Service command code 93 terminates this program
+    svc 0                 // Call linux to terminate the program
