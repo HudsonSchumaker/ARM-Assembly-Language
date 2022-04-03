5ba6619ee3707fd4048abc1f1b2e95414e167666
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-04-03 12:50:19 +0200

    Update main.c

--- code/c-call-function/macos/main.c
+++ code/c-call-function/macos/main.c
@@ -1,1 +1,23 @@
+//
+//  main.c
+//  Lab
+//
+//  Created by Hudson Schumaker on 26.03.22.
+//
 
+#include <stdio.h>
+#include "calc.h"
+
+int main(int argc, const char * argv[]) {
+    
+    int x = add(3, 2);
+    printf("%d\n", x);
+
+    x = sub(3, 2);
+    printf("%d\n", x);
+    
+    x = area_square(2);
+    printf("%d\n", x);
+    
+    return 0;
+}
