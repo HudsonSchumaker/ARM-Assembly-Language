f49af6bc01a086e702fddb0874e0c80ffc6dbb4a
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-04-03 12:51:11 +0200

    Update calc.h

--- code/c-call-function/macos/calc.h
+++ code/c-call-function/macos/calc.h
@@ -1,1 +1,15 @@
+//
+//  calc.h
+//  Lab
+//
+//  Created by Hudson Schumaker on 26.03.22.
+//
 
+#ifndef calc_h
+#define calc_h
+
+extern int add(int a, int b);
+extern int sub(int a, int b);
+extern int area_square(int a);
+
+#endif /* calc_h */
