67dbbe3dd99ba3f46eb807050bc1ef0eaeeee000
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-04-03 12:51:31 +0200

    Update calc.s

--- code/c-call-function/macos/calc.s
+++ code/c-call-function/macos/calc.s
@@ -1,1 +1,25 @@
+//
+//  Calc.s
+//  Lab
+//
+//  Created by Hudson Schumaker on 26.03.22.
+//
 
+.text
+
+.global _add, _sub, _area_square
+.align 4
+
+_add:
+    ADD  x0, x0, x1
+    RET
+
+_sub:
+    SUB x0, x0, x1
+    RET
+
+_area_square:
+    MUL x0, x0, x0
+    RET
+
+.data
