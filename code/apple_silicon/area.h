13369fb85a82096e9743e3d700bcaad1c98f10f3
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-07-29 20:23:26 +0200

    Create area.h

---
+++ code/apple_silicon/area.h
@@ -0,0 +1,17 @@
+//
+//  area.h
+//  Lab
+//
+//  Created by Hudson Schumaker on 03.04.22.
+//
+
+#ifndef area_h
+#define area_h
+
+extern float area_square(float s);
+extern float area_rectangle(float l, float w);
+extern float area_triangle(float b, float h);
+extern float area_parallelogram(float b, float h);
+extern float area_trapezoid(float b,  float B, float h);
+
+#endif /* area_h */
