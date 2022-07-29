0b32a41566ed52fb1337d9c48dab48f3259dba06
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-07-29 20:25:39 +0200

    Create area.s

---
+++ code/apple_silicon/area.s
@@ -0,0 +1,28 @@
+//
+//  area.s
+//  Lab
+//
+//  Created by Hudson Schumaker on 03.04.22.
+//
+
+.text
+
+.global _area_square, _area_rectangle, _area_triangle
+.align 4
+
+_area_square:
+    FMUL s0, s0, s0
+    RET
+
+_area_rectangle:
+    FMUL s0, s0, s1
+    RET
+
+_area_triangle:
+    FMUL s0, s0, s1
+    FMOV s8, #2
+    FDIV s0, s0, s8
+    RET
+
+.data
+    pi: .single 3.14159
