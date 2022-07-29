58741bc33b744c397626dfd7051d5302d574cea1
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-07-29 20:29:04 +0200

    Create main.c

---
+++ code/apple_silicon/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Lab
+//
+
+#include <stdio.h>
+#include "area.h"
+
+int main(int argc, const char * argv[]) {
+        
+    float z = area_square(2.0f);
+    printf("%f\n", z);
+    
+    z = area_triangle(2.0f, 3.0f);
+    printf("%f\n", z);
+    
+    return 0;
+}
